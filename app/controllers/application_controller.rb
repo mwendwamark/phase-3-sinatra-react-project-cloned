de9f11d21b9d75724bc4c1c023e262cc0235077e
mark mwendwa <mwendwamark310@gmail.com>
2023-06-08 09:35:09 +0300

    add routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,72 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  get "/estates" do
+    houses = Estate.all
+    houses.to_json
   end
 
+  get "/blogs" do
+    content = Blog.all
+    content.to_json
+  end
+
+  get "/owners" do
+    owners = Owner.all
+    owners.to_json
+  end
+
+  post "/estates" do
+    house = Estate.create(
+      location: params[:location],
+      image_url: params[:image_url],
+      description: params[:description],
+      rent: params[:rent],
+    )
+  end
+
+  post "/owners" do
+    owner = Owner.create(
+      name: params[:name],
+      email: params[:email],
+    )
+  end
+
+  post "/blogs" do
+    blog = Blog.create(
+      comments: params[:comments],
+      ratings: params[:ratings],
+      likes: params[:likes]
+    )
+  end
+
+  patch "/owners/:id" do
+    owner = Owner.find(params[:id])
+    owner.update(
+      name: params[:name],
+      email: params[:email],
+    )
+    owner.to_json
+  end
+
+  patch "/blogs/:id" do 
+    blog = Blog.find(params[:id])
+    blog.update(
+      likes: params[:likes]
+    )
+  end
+
+  delete "/blogs/:id" do
+    blog = Blog.find(params[:id])
+    blog.delete
+    blog.to_json
+  end
+
+  delete "/estates/:id" do 
+    house = Estate.find(params[:id])
+    house.delete
+    house.to_json
+  end
 end
